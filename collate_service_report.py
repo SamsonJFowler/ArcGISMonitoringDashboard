313b2acc65315f244a66efdb5d89e6713cbe38aa
Sam Fowler <96063019+SamsonJFowler@users.noreply.github.com>
2022-03-11 16:58:45 +0000

    script redis

---
+++ collate_service_report.py
@@ -0,0 +1,9 @@
+"""
+author: samfowler created: 2022-03-09
+"""
+
+from arcgis.gis import GIS
+
+#TODO - reports and iis functions, visual outputs.
+
+
---
+++ service_reporting.py
@@ -0,0 +1,4 @@
+---
+author: samfowler created: 2022-03-09
+---
+#TODO -> dashboard reporting,  
